388843cec67e7813d64b436c5e37355661a0ac07
paulakash36 <paulakash36@gmail.com>
2020-08-16 04:31:10 +0530

    React Router

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -6,7 +6,7 @@ function RenderMenuItem ({dish,onClick}) {
     
     return (
         <div key={dish.id} className="col-12 col-md-5 m-1">
-        <Card onClick={() => this.props.onClick(dish.id)}>
+        <Card onClick={(props) => props.onClick(dish.id)}>
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
                 <CardTitle>{dish.name}</CardTitle>
