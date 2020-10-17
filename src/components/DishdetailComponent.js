127f47679f3f91cd3bf58fb2aaa16564f91191fe
paulakash36 <paulakash36@gmail.com>
2020-10-18 00:34:23 +0530

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import {LocalForm, Control, Errors} from 'react-redux-form';
 import {Loading} from './LoadingComponent';
 import {baseUrl} from '../shared/baseUrl';
+import {FadeTransform, Fade, Stagger} from 'react-animation-components';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || val.length <= len;
@@ -119,13 +120,17 @@ class CommentForm extends Component {
             const cmnts = comments.map((comment) => {
                 return(
                     <ul key={comment.id} className='list-unstyled'>
-                        <li >
-                            <p>{comment.comment}</p>
-                            <p>-- {comment.author}, 
-                            &nbsp;
-                            {new Intl.DateTimeFormat('en-US',{year: 'numeric', month:'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                            </p>
+                        <Stagger in>
+                          <Fade in>
+                            <li key={comment.id} >
+                              <p>{comment.comment}</p>
+                              <p>-- {comment.author}, 
+                              &nbsp;
+                              {new Intl.DateTimeFormat('en-US',{year: 'numeric', month:'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                              </p>
                         </li>
+                        </Fade>
+                        </Stagger>
                     </ul>
                     );
             });
@@ -146,6 +151,10 @@ class CommentForm extends Component {
         if (dish!= null){
             return(
                 <div className="col-12 col-md-5 m-1">
+                     <FadeTransform in 
+                        transformProps={{
+                            exitTransform: 'scale(0.5) translateY(-50%)'
+                        }}>
                     <Card>
                         <CardImg top width="100%" src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
@@ -153,6 +162,7 @@ class CommentForm extends Component {
                             <CardText>{dish.description}</CardText>
                         </CardBody>
                     </Card>
+                    </FadeTransform>
                 </div>
             );
         }
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle} from 'reactstrap';
 import {Loading} from './LoadingComponent';
 import {baseUrl} from '../shared/baseUrl';
-
+import { FadeTransform } from 'react-animation-components';
 
 
 function RenderCard({item, isLoading, errMess}) {
@@ -18,6 +18,10 @@ function RenderCard({item, isLoading, errMess}) {
     }
     else 
     return(
+        <FadeTransform in 
+            transformProps={{
+                exitTransform: 'scale(0.5) translateY(-50%)'
+            }}>
         <Card>
             <CardImg src={ baseUrl + item.image} alt={item.name} />
             <CardBody>
@@ -26,6 +30,7 @@ function RenderCard({item, isLoading, errMess}) {
             <CardText>{item.description}</CardText>
             </CardBody>
         </Card>
+        </FadeTransform>
         );
 
     }
